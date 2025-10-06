aa78b9e57e7e37c79142b289f972183f7129edac
Mihaivictor5 <92265370+Mihaivictor5@users.noreply.github.com>
2025-10-06 11:22:56 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+/*
+ * HelloWorld.c
+ *
+ * Created: 9/9/2025 10:43:27 AM
+ * Author : Alin
+ */ 
+
+
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "usart.h"
+
+int main(void) {  
+
+    
+
+  uart_init(); // open the communication to the microcontroller
+  io_redirect(); // redirect input and output to the communication
+
+    
+  while(1) {
+		
+	  printf("Hello World \n");
+	  _delay_ms(1000);
+
+  }
+  
+  return 0;
+}
